f578f747cab71f27a736d55de4aec139ab8152fe
Gary Newton <Gary Newton>
2017-04-20 12:37:08 -0400

    installed npm packages, added twitter tokens, updated liri.js

--- keys.js
+++ keys.js
@@ -2,9 +2,9 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<dFP2NiIUJ03hArj45iUtd5bC8>',
+  consumer_secret: '<f0CJXgAxvQ0SqlQQM99uoSPc0Qe7fyVb8OFeEt1A0T7Z7MIOYM>',
+  access_token_key: '<f0CJXgAxvQ0SqlQQM99uoSPc0Qe7fyVb8OFeEt1A0T7Z7MIOYM>',
+  access_token_secret: '<OXflCe54rtwQDYBliO63aZWZCzyu2nEbMUUVh8pUgS8a6>',
 }
 ```
