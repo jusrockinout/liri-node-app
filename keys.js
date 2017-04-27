22c4d4b7c6720929cb9ebf072c5bac7388f51324
Gary Newton <Gary Newton>
2017-04-27 15:23:42 -0400

    Finishing touches on liri.js, misc directories and dependencies updated.

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
-```JavaScript
-console.log('this is loaded');
-
 exports.twitterKeys = {
-  consumer_key: '<dFP2NiIUJ03hArj45iUtd5bC8>',
-  consumer_secret: '<f0CJXgAxvQ0SqlQQM99uoSPc0Qe7fyVb8OFeEt1A0T7Z7MIOYM>',
-  access_token_key: '<f0CJXgAxvQ0SqlQQM99uoSPc0Qe7fyVb8OFeEt1A0T7Z7MIOYM>',
-  access_token_secret: '<OXflCe54rtwQDYBliO63aZWZCzyu2nEbMUUVh8pUgS8a6>',
-}
-```
+  consumer_key: 'dFP2NiIUJ03hArj45iUtd5bC8',
+  consumer_secret: 'f0CJXgAxvQ0SqlQQM99uoSPc0Qe7fyVb8OFeEt1A0T7Z7MIOYM',
+  access_token_key: '714903020-3BG1jEibFOAvCQ9odh9lFDwBp8Z3e3dOmjXyZeiz',
+  access_token_secret: 'OXflCe54rtwQDYBliO63aZWZCzyu2nEbMUUVh8pUgS8a6',
+};
--- liri.js
+++ liri.js
@@ -1,74 +1,116 @@
-   // * [Twitter](https://www.npmjs.com/package/twitter)
-   // * [Spotify](https://www.npmjs.com/package/spotify)
-   // * [Request](https://www.npmjs.com/package/request)
-   // * You'll use Request to grab data from the [OMDB API](http://www.omdbapi.com).
-
-// 7. Make it so liri.js can take in one of the following commands:
-
-//    * `my-tweets`
-
-//    * `spotify-this-song`
-
-//    * `movie-this`
-
-//    * `do-what-it-says`
-
-// ### What Each Command Should Do
-
-// 1. `node liri.js my-tweets`
-
-//    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-
-//    * if no song is provided then your program will default to
-//      * "The Sign" by Ace of Base
-
-// 3. `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//        * Rotten Tomatoes Rating.
-//        * Rotten Tomatoes URL.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-//      * It's on Netflix!
-
-// 4. `node liri.js do-what-it-says`
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Feel free to change the text in that document to test out the feature for other commands.
-
-// ### BONUS
-
 // * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-
 // * Make sure you append each command you run to the `log.txt` file. 
-
 // * Do not overwrite your file each time you run a command.
 
+var key = require('./keys.js');
+var fs = require("fs");
+var event = process.argv[2];
+var globe; //Used for either movie or song 
+
+function liri(method, param) {
+	switch(method){
+		
+		case "my-tweets": /*This is what you type in command-line after node liri.js my-tweets */
+		twitterProfile();
+		break;
+
+		case "spotify-this-song":
+		spotifySong(param);
+		break;
+
+		case "movie-this":
+		movieData(param);
+		break;
+
+		case "do-what-it-says":
+		doIt();
+		break;
+
+	} //Closes out Switch event 
+}
+/*Create function to get tweets from profile*/
+function twitterProfile(){
+	/*Use twitter variable to store the twitter API data*/
+	var Twitter = require("twitter");
+	/*Use client variable to store twitter keys*/
+	var client = new Twitter(key.twitterKeys);
+
+	var params = {screen_name: "jusrockinout"};
+	client.get('statuses/user_timeline', {count:20}, function(error,tweets,response){
+		if(!error){
+			console.log("Here are your last 20 tweets:");
+			for(var i = 0; i<tweets.length; i++){
+				console.log(tweets[i].text);
+				console.log(tweets[i].created_at);
+				console.log("");
+				} //Closes out for-loop 
+			} //Closes out if statement 
+
+			else {
+				console.log(error);
+			}
+			
+		}); //Closes out function 
+}
+
+function spotifySong(param){
+	var spotify = require("spotify");
+	var song = param || "The Sign Ace of Base";
+	spotify.search({type: "track", query: song}, function(err, data){
+
+		if(err){
+			console.log('Error occurred: ' + err);
+			return;
+			} //Closes if statement 
+			else {
+				var logString = "";
+				if(data.tracks.items.length > 0){
+					logString += "Artist name: " + data.tracks.items[0].artists[0].name + "\n";
+					console.log("Artist name: " + data.tracks.items[0].artists[0].name);
+					logString += "Song: " + data.tracks.items[0].name + "\n";
+					console.log("Song: " + data.tracks.items[0].name);
+					logString += "Song: " + data.tracks.items[0].name + "\n";
+					console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+					console.log("Album: " + data.tracks.items[0].album.name);
+				} //Closes if statement for data.tracks.items.length
+				appendLog(logString);
+			} //Closes else statement
+		}); //Closes out spotify.search 
+	}//Closes out spotifySong function 
 
 
+	function movieData(param){
+		var request = require("request");
+		var movie = param || "Mr Nobody"
+		movie = movie.split(" ").join("+");
+		request('http://www.omdbapi.com/?t=' + movie + "&tomatoes=true&plot=short", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+		    	console.log(body) // Show the HTML for the Google homepage. 
+		 		 } //Closes out if statement 
+		 		 var data = JSON.parse(body);
+		 		 console.log("Movie title: " + data.Title);
+		 		 console.log("Year made: " + data.Year);
+		 		 console.log("IMDB Rating: " + data.imdbRating);
+		 		 console.log("Country: " + data.Country);
+		 		 console.log("Language: " + data.Language);
+		 		 console.log("Plot: " + data.Plot);
+		 		 console.log("Actors: " + data.Actors);
+		 		 console.log("Rotten Tomatoes rating: " + data.tomatoUserRating);
+		 		 console.log("Rotten Tomatoes URL: " + data.tomatoURL);
+		}) //Closes out request function  
+	}; //Closes out movieData function
 
+	
+	function doIt() {
+		fs.readFile("random.txt", "UTF8", function(err,data){
+			if(err){
+				console.log("Bummer.There's an error: " + err);
+			}
 
-require ("keys.js");
+			var dataArray = data.split(',');
+			liri(dataArray[0], dataArray[1]);
+		});
 
-var keysData;
+	}
 
-
+	liri(process.argv[2], process.argv[3]);
